283d4c5fb15a57612e13a6036a092b97e55c96e3
Shivang-Bajaj30 <shivang.bajaj30@gmail.com>
2025-02-12 00:07:23 +0530

    Initial Git Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
+    const content = document.getElementById('pageContent');
     const toggleButton = document.getElementById('toggleButton');
+
+    // Toggle classes
     menu.classList.toggle('open');
+    content.classList.toggle('shifted');
     toggleButton.classList.toggle('active');
 }
+
+const filterMenu = document.getElementById('filterMenu');
+const closeButton = document.getElementById('closeButton');
+
+closeButton.addEventListener('click', () => {
+    filterMenu.classList.toggle('hidden');
+});
